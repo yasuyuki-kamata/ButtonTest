80173dc8f802e14f86466177ee41786613de1f9d
Yasuyuki Kamata <flandre.s199@gmail.com>
2015-02-25 01:30:26 +0900

    first commit

---
+++ Assets/ToggleButtonSample_Move/Scripts/EnableChecker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class EnableChecker : MonoBehaviour
+{
+	private Animator anim;
+	private Text text;
+
+	void Start () {
+		anim = GameObject.Find ("Button2").GetComponent<Animator>();
+		text = GetComponent<Text> ();
+	}
+
+	void Update () {
+		ApplyText ();
+	}
+
+	public void ApplyText () {
+		text.text = anim.GetBool ("isEnable").ToString();
+	}
+}
---
+++ Assets/ToggleButtonSample_Move/Scripts/MoveSampleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSampleController : MonoBehaviour
+{
+	private Animator anim;
+	private int param;
+
+	void Start () {
+		anim = GameObject.Find ("Button2").GetComponent<Animator> ();
+		param = Animator.StringToHash ("isEnable");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool(param,  !anim.GetBool(param));
+		}
+	}
+}
